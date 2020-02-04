25dd70ee219ccb37c32406f1407f94a14ec67190
Stejara Dinulescu <stejara@gmail.com>
2020-02-04 11:14:00 -0800

    particles-remix.cpp complete

--- assignment/particles-remix.cpp
+++ assignment/particles-remix.cpp
@@ -1,7 +1,10 @@
 /* This is Rodney's particles-p5.cpp.
  * File path: rodneydup/Assignment2/particles-p5.cpp. 
  * Rodney's original file shows a solar system in orbit around the sun
- * Stejara's changes: tbd
+ * Stejara's changes: 1. mass relationships
+ *                    2. textcoord
+ *                    3. taking square root instead of distance cubed in gravity calculation
+ *                    4. acceleration relationship between i and j ("gravity")
  */
 
 
@@ -19,11 +22,11 @@ string slurp(string fileName); // forward declaration
 
 struct AlloApp : App {
   // add more GUI here
-  Parameter pointSize{"/pointSize", "", 2.0, "", 0.0, 2.0};
-  Parameter timeStep{"/timeStep", "", 0.01, "", 0.001, 0.1};
+  Parameter pointSize{"/pointSize", "", 0.6, "", 0.0, 2.0};
+  Parameter timeStep{"/timeStep", "", 0.04, "", 0.001, 0.1};
   Parameter dragFactor{"/drag", "", 0.01, "", 0, 0.5};
-  ParameterBool drawTrails{"/drawTrails", "", 0};
-  Parameter asymmetry{"/asymmetry", "", 0, "", 0, 1};
+  ParameterBool drawTrails{"/drawTrails", "", 1};
+  Parameter asymmetry{"/asymmetry", "", 0.3, "", 0, 1};
 
   ControlGUI gui;
 
@@ -41,13 +44,6 @@ struct AlloApp : App {
   const float GM =
       1.190588106 * pow(10, -5); // Geocentric gravitational constant
                                  // (gravity per earth mass) in AU^3/s^2
-  const float largeBodies[34] = {
-      // Geocentric masses of 34 largest bodies
-      333000,   317.83,   95.162,   14.536,   17.147,  1,       0.815,
-      0.107,    0.0248,   0.0225,   0.0553,   0.018,   0.015,   0.0123,
-      0.008035, 0.003599, 0.0022,   0.0028,   0.00059, 0.00066, 0.00039,
-      0.0005,   0.00033,  0.00053,  0.00029,  0.00025, 0.0002,  0.000226,
-      0.000183, 0.0002,   0.000103, 0.000157, 0.0001,  0.000082};
 
   const float planetDistance[10] = {0, 5.20,  9.58, 19.2,  30.1,
                                     1, 0.722, 1.52, 0.387, 39.5};
@@ -74,7 +70,7 @@ struct AlloApp : App {
 
     reset();
 
-    nav().pos(0, 0, 15);
+    nav().pos(3, 0, 13);
   }
 
   void reset() {
@@ -92,33 +88,22 @@ struct AlloApp : App {
     };
 
     for (int i = 0; i < 1000; i++) {
-      if (i == 0) {
-        mesh.color(HSV(0.125, 1, 1));
-      } else if (i < 10) {
-        mesh.color(rc());
+      mesh.color(rc());
 
-      } else if (i > 10) {
-        mesh.color(HSV(0, 0, 0.7));
-      }
-
-      // float m = rnd::uniform(3.0, 0.5);
-      //   float m = 3 + rnd::normal() / 2;
-      //   if (m < 0.5) m = 0.5;
       float m = 0;
 
       if (i < 34) {
-        m = largeBodies[i];
+        m = rnd::uniform(33000);
       } else if (34 <= i < 100) {
-        m = rnd::uniform(0.00001) + 0.00001;
+        m = rnd::uniform(100) + 0.1;
       } else if (100 <= i < 500) {
-        m = rnd::uniform(0.0000077) + 0.0000001;
+        m = rnd::uniform(0.1) + 0.01;
       } else if (500 <= i < 1000) {
-        m = rnd::uniform(0.00000001) + 0.00000000001;
+        m = rnd::uniform(0.00001) + 0.0000001;
       }
 
       mass.push_back(m);
 
-      // separate state arrays
       if (i == 0) {
         mesh.vertex(0);
         velocity.push_back(rv(0));
@@ -132,14 +117,8 @@ struct AlloApp : App {
         acceleration.push_back(0);
       }
 
-      // using a simplified volume/size relationship
-      if (i == 0) {
-        mesh.texCoord(20, 0); // s, t
-      } else {
-        float s = pow(m, 1.0 / 3);
-        mesh.texCoord(s > 0.4 ? s : 0.4, 0); // s, t
-      }
-    }
+        mesh.texCoord(pow(m, 1.0f / 3), 0); 
+     }
   }
 
   bool freeze = false;
@@ -149,35 +128,21 @@ struct AlloApp : App {
 
     // ignore the real dt and set the time step;
     dt = timeStep;
-    // Calculate forces
+
+    // Calculate gravitational force
     for (int i = 0; i < velocity.size(); i++) {
-      if (mass[i] == 0)
-        continue;
-      // drag
-      // acceleration[i] -= velocity[i] * dragFactor;
-      // Inter-particle gravitation
       for (int j = i + 1; j < velocity.size(); j++) {
-        Vec3f r = mesh.vertices()[i] - mesh.vertices()[j];
-        if (mass[j] == 0)
-          continue;
-        if (r.mag() < 0.01) {
-          mass[i] += mass[j];
-          mass[j] = 0;
-          mesh.vertices()[j] = 0;
-          float s = pow(mass[i], 1.0 / 3);
-          mesh.texCoord2s()[i] = Vec2f(s > 0.4 ? s : 0.4, 0);
-        }
-        float distcubed = pow(r.mag(), 3.0);
+        Vec3f r = mesh.vertices()[j] - mesh.vertices()[i];
+        float distcubed = pow(r.mag(), -2.0);
         if (distcubed == 0)
           continue;
-        Vec3f F = GM * mass[j] * mass[i] * r / distcubed;
-        acceleration[i] -= F * (1 + float(asymmetry));
-        acceleration[j] += F / (1 + float(asymmetry));
+        Vec3f F = GM * mass[j] * mass[i] * r.normalize() / distcubed;
+        acceleration[i] += F * float(asymmetry * 0.1);
+        acceleration[j] -= F * float(asymmetry);
       }
     }
 
     // Integration
-    //
     vector<Vec3f> &position(mesh.vertices());
     for (int i = 0; i < velocity.size(); i++) {
       // "backward" Euler integration
@@ -200,7 +165,6 @@ struct AlloApp : App {
     }
 
     if (k.key() == 'r') {
-      //
       reset();
     }
 
