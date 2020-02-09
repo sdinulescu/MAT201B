0f338663a43902052eece811f8edf8001fe671fe
Stejara Dinulescu <stejara@gmail.com>
2020-02-09 15:15:46 -0800

    alignment and cohesion done (I think?)

--- assignment/boids.cpp
+++ assignment/boids.cpp
@@ -26,7 +26,8 @@ struct AlloApp : App {
   // add more GUI here
   Parameter moveRate{"/moveRate", "", 1.0, "", 0.0, 2.0};
   Parameter turnRate{"/turnRate", "", 1.0, "", 0.0, 2.0};
-  Parameter localRadius{"/localRadius", "", 0.4, "", 0.01, 0.9};
+  Parameter localRadius{"/localRadius", "", 0.08, "", 0.01, 0.9};
+  Parameter separationDistance{"/separationDistance", "", 0.01, "", 0.01, 0.9};
   Parameter size{"/size", "", 1.0, "", 0.0, 2.0};
   Parameter ratio{"/ratio", "", 1.0, "", 0.0, 2.0};
   ControlGUI gui;
@@ -38,9 +39,16 @@ struct AlloApp : App {
 
   int agentNum = 1000;
 
+  void reset() {
+    for (int i = 0; i < agentNum; i++) {
+      agent[i].pos(rv());
+      agent[i].faceToward(rv());
+    }
+  }
+
   void onCreate() override {
     // add more GUI here
-    gui << moveRate << turnRate << localRadius << size << ratio;
+    gui << moveRate << turnRate << localRadius << separationDistance << size << ratio;
     gui.init();
     navControl().useMouse(false);
 
@@ -62,35 +70,48 @@ struct AlloApp : App {
       const Vec3f& up(a.uu());
       mesh.color(up.x, up.y, up.z);
     }
-
     nav().pos(0, 0, 10);
   }
 
   void onAnimate(double dt) override {
-    // for each pair of agents
-    for (unsigned i = 0; i < agentNum; i++)
+    //calculate stuff
+    Vec3f avgHeading(0, 0, 0);
+    Vec3f centerPos(0, 0, 0);
+    for (unsigned i = 0; i < agentNum; i++) {
+      agent[i].flockCount = 0; //reset flock count
       for (unsigned j = 1 + i; j < agentNum; j++) {
         float distance = (agent[j].pos() - agent[i].pos()).mag();
-        if (distance < localRadius) {
-          // put code here
-          //
-        }
+        
+        if (distance < localRadius) { 
+          //calculate alignment and cohesion vals if flock mates are far enough away from each other
+          agent[i].flockCount++; //increase the flockmate count for that specific agent
+          if (distance < separationDistance) {
+            //cout << "separate" << endl;
+            avgHeading -= agent[j].uf();
+          } else {
+            avgHeading += agent[j].uf();
+            centerPos += agent[j].pos();
+          }
+        } 
       }
 
-    // only once the above loop is done do we have good data on average headings and centers
+      //cout << agent[i].flockCount << endl;
+      if (agent[i].flockCount > 0) {
+        agent[i].heading = avgHeading.normalize()/agent[i].flockCount;
+        agent[i].center = centerPos.normalize()/agent[i].flockCount;
+      }
 
-    //
-    // put code here
-    //
-
-    // point agents in random direction (REMOVE THIS CODE)
-    for (unsigned i = 0; i < agentNum; i++) {
-      agent[i].faceToward(rv(), 0.03 * turnRate);
+      //cout << agent[i].heading << " " << agent[i].center << endl;
     }
-
-    // move the agents along (KEEP THIS CODE)
+    // only once the above loop is done do we have good data on average headings and centers
+    // here is where we actually point them in the right direction and move them
+   
     for (unsigned i = 0; i < agentNum; i++) {
-      agent[i].pos() += agent[i].uf() * moveRate * 0.002;
+      //alignment
+      agent[i].faceToward(agent[i].heading); // point agents in the direction of their heading
+      //cohesion
+      agent[i].pos().lerp(agent[i].center, moveRate * 0.02);
+      //agent[i].pos() += agent[i].uf() * moveRate * 0.002; //move the agents along
     }
 
     // respawn agents if they go too far (MAYBE KEEP)
@@ -101,7 +122,7 @@ struct AlloApp : App {
       }
     }
 
-    // visualize the agents
+    // visualize the agents, update meshes
     vector<Vec3f>& v(mesh.vertices());
     vector<Vec3f>& n(mesh.normals());
     vector<Color>& c(mesh.colors());
@@ -111,6 +132,13 @@ struct AlloApp : App {
       const Vec3d& up(agent[i].uu());
       c[i].set(up.x, up.y, up.z);
     }
+  }
+
+  bool onKeyDown(const Keyboard& k) override {
+    if (k.key() == 'r') {
+      reset();
+    }
+    return true;
   }
 
   void onDraw(Graphics& g) override {
@@ -125,6 +153,8 @@ struct AlloApp : App {
     gui.draw(g);
   }
 };
+
+
 
 int main() { AlloApp().start(); }
 
